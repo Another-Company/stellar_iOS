edb94c312f1c424f37d3e22f82f52edef237bc07
gyugyu90 <kh2000park@gmail.com>
2018-02-25 13:06:02 +0900

    add sample models

---
+++ Stellar_iOS/Manager/AuthManager.swift
@@ -0,0 +1,13 @@
+//
+//  AuthManager.swift
+//  Stellar_iOS
+//
+//  Created by khpark on 2018. 2. 25..
+//  Copyright © 2018년 AnotherCompany. All rights reserved.
+//
+
+import UIKit
+
+class AuthManager: NSObject {
+
+}
---
+++ Stellar_iOS/Model/Response/SomeResponse.swift
@@ -0,0 +1,12 @@
+//
+//  SomeResponse.swift
+//  
+//
+//  Created by khpark on 2018. 2. 25..
+//
+
+import UIKit
+
+class SomeResponse: NSObject {
+
+}
